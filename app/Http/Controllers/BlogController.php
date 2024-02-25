ddf2d1abe2232eb471d0457fdbb1346d5bb6b5ec
DhoonWaiYanMin <dhoon166@gmail.com>
2024-02-25 13:00:01 +0630

    update and delete blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,13 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Models\Blog;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class BlogController extends Controller
 {
-    public function detail($id){
+    public function show($id){
         $blog = Blog::where("id",$id)->first();
         return view("blog.detail",['blog'=>$blog]);
+    }
+
+    public function edit($id){
+        $blog = Blog::where("id",$id)->first();
+        $categories = Category::all();
+        return view("blog.edit",['blog'=>$blog , 'categories' => $categories]);
+    }
+
+    public function destroy(Blog $blog){
+        $blog->delete();
+
+        return redirect("profile");
     }
 }
